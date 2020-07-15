d3d54b8c2d4d80f3f97f3d9cc445b3335cf5b796
Xetera <mythologyali@gmail.com>
2020-07-15 09:51:42 -0700

    fixed build errors

--- web/gatsby-ssr.js
+++ web/gatsby-ssr.js
@@ -5,6 +5,8 @@
  */
 
 // You can delete this file if you're not using it
+import React from "react";
+import Layout from "./src/components/Layout/Layout";
 
 export const wrapPageElement = ({ element, props }) => {
   return <Layout {...props}>{element}</Layout>;
